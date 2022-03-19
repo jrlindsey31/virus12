19ce69094f0a5a34fc9dd513894774317dc6d690
jrlindsey31 <80177528+jrlindsey31@users.noreply.github.com>
2022-03-18 23:20:52 -0400

    Delete c directory

--- c/virus.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.startfile("C:\Windows\System32\cmd.exe") # Open any program, text or office document
-
-import ctypes
-ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\Users\rick\Desktop\c\backgrounds.jpg" , 0)
